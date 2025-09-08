a5a5c19f8ce9deff936decaaf4503e2d1ea31e2c
dripsolutions <fcsteikee@gmail.com>
2025-09-08 20:52:41 -0300

    Update Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -1,84 +1,82 @@
  allowedUsers = {
-    -- 📷 Criador de conteúdo
-      ["KNZINN_NOVAMENTE"] = true, -- (novo divulgador ofc) 
-      ["FFS1L7d3"] = true, -- (Novo divulgador)
-      ["Aloysioerick"] = true, -- chiagoremastered    
+    --  Criadores de conteúdo 🔮
+    ["KNZINN_NOVAMENTE"]     = true, -- novo divulgador
+    ["FFS1L7d3"]             = true, -- novo divulgador
+    ["Aloysioerick"]         = true, -- chiagoremastered
 
-    -- 🛠️ Staff 
-    ["Roblox_Anthex"] = true,
-    ["RUANRODRIGO121121"] = true,
-    ["magnusfxp"] = true,
-    ["roblox_freitas24"] = true,
-  
-    -- 🎟️ Temporários
-    ["nz_stylekkj"] = true, -- (07/09 ate 11/09)
-    ["kheuj2bsqjhwhwy"] = true, -- (07/09 ate 10/09) 3 dias
-    ["Aliban_32"] = true, -- (05/09 ate 08/09) 3 dias
-    ["papanoelpro12342"] = true, -- (04/09 ate 20/09) 16 dias de painel
-    ["spyderSammdy"] = true, -- (03/09 ate 11/09)
-    ["XxRE1xX3"] = true, -- (03/09 ate 07/09) 4 dias de paindl
-    ["ERIK90909082"] = true, -- (02/09 ate 01/11) 2 meses de painel
-    ["toto12348802"] = true, -- (30/08 ate 15/09)
-    ["x_P3drox"] = true, -- (30/08 ate 16/09) 16 dias de painel
-    ["mendrake_253"] = true, -- (30/08 ate 23/09) 23 dias de painel 
-    ["MiguelBert2014"] = true, -- (30/08 ate 08/09) 8 dias de painel
-    ["lucas02sjsj"] = true, -- (14/08 até 14/09) 1 mês de painel
-    ["cleitinho_adm"] = true, -- (15/08 até 15/09) 1 mês de painel
-    ["Aloysioerick"] = true, -- (17/08 até 17/10) 2 meses de painel
-    ["Miguel_kiwi777"] = true, -- (18/08 até 18/09) 2 meses de painel
-    ["vivi18197"] = true, -- até 24 de setembro
-    ["huzako7"] = true, -- até 24 de setembro
-    ["BLSOLTEIRO"] = true, -- até 21 de setembro
-    ["kaiserfogo9"] = true, -- até 21 de setembro
-    ["woonz_x"] = true, -- Até 24 Setm
-    ["twelo_zinha3"] = true, -- (26/08 até 26/09)
-    ["Gabrielmilitar_6"] = true, -- (27/08 até 27/12)
-    ["Riggs_scott"] = true, -- 26/08 ate 26/09
-    ["01Xxx09"] = true, -- 29/08 até 12/09
-    ["almahola9"] = true, -- (29/08 até 21/01/2026) 
-    ["iwejjbsb1"] = true, -- (01/09 ate 01/01/26) 4 meses de painel
-    ["WONEVERY1"] = true, -- (02/09 até 08/09)
-    ["mandrake07970"] = true, -- (03/09 até 23/09) 20 dias de painel
-    ["mini_fofinhodemais"] = true, -- (05/09 até 08/09) 3 dia de painel
-    ["MANDRAK123459"] = true, -- (05/09 até 08/09) 3 dias painel
-    ["biel_mrqs"] = true, --(06/09 ate 6 de janeiro)
-    ["BloxLOUX_YT"] = true, --(06/09 ate 09/09)
-    ["CRIA027K"] = true, --(07/09 até 19/09)  
-    ["berluiz2001"] = true, --(07/09 até 07/11) 2 meses de painel 
-    ["12etdfgj"] = true, -- (07/09 até 14/09) 7/6 dias de painel 
-    ["ggmax10t"] = true, -- (07/09 até 07/10) 1 mês de painel
-    ["mellu107"] = true, -- (07/09 ate 10/09) -- 3 dias
-    ["rylyn15243w0"] = true, -- (07/09 10/09) -- 3
-    ["MasterReaper111"] = true, -- (08/09 até 08/01) 4mss
-    ["LuisCarlos_lcs"] = true, -- (08/09 até 11/09)
-    ["pabinhoplay"] = true, -- 1 dia
-    ["Theozinhodomorro"] = true, --(08/09 até 11/09)
- 
-    -- ✅ Permanentes
-    ["botinha_W"] = true, 
-    ["21qxkg"] = true, 
-    ["shotxz_cria"] = true,
-    ["Victhomxd"] = true, 
-    ["aminos_055"] = true, 
-    ["llenado123456"] = true,
-    ["smulem_01"] = true,
-    ["Pedrocamandrake391"] = true,
-    ["KIRA7KILLAS"] = true,
-    ["daviricardo25022"] = true,
-    ["Salazar123xxx2"] = true,
-    ["errorpro1d"] = true,
-    ["AOEX_FH"] = true,
-    ["contasegudariadesv"] = true,
-    ["zoro_fenix42"] = true,
-    ["pozella999"] = true,
-    ["nikz68"] = true,
-    ["kayro20112p"] = true,
-    ["emanifesto6q"] = true,
-    ["Lolyta6662"] = true,
-    ["Migxzz15"] = true,
-    ["sparta9842"] = true,
-    ["MENOR_RSA"] = true,
-    ["dvdfy4wfiyf"] = true,
+    --  Moderadores 📍
+    ["Roblox_Anthex"]        = true,
+    ["RUANRODRIGO121121"]    = true,
+    ["magnusfxp"]            = true,
+
+    --  Tempos Estimados 📌
+    ["nz_stylekkj"]          = true, -- 07/09 até 11/09
+    ["kheuj2bsqjhwhwy"]      = true, -- 07/09 até 10/09
+    ["Aliban_32"]            = true, -- 05/09 até 08/09
+    ["papanoelpro12342"]     = true, -- 04/09 até 20/09
+    ["spyderSammdy"]         = true, -- 03/09 até 11/09
+    ["XxRE1xX3"]             = true, -- 03/09 até 07/09
+    ["ERIK90909082"]         = true, -- 02/09 até 01/11
+    ["toto12348802"]         = true, -- 30/08 até 15/09
+    ["x_P3drox"]             = true, -- 30/08 até 16/09
+    ["mendrake_253"]         = true, -- 30/08 até 23/09
+    ["MiguelBert2014"]       = true, -- 30/08 até 08/09
+    ["lucas02sjsj"]          = true, -- 14/08 até 14/09
+    ["cleitinho_adm"]        = true, -- 15/08 até 15/09
+    ["Miguel_kiwi777"]       = true, -- 18/08 até 18/09
+    ["vivi18197"]            = true, -- até 24/09
+    ["huzako7"]              = true, -- até 24/09
+    ["BLSOLTEIRO"]           = true, -- até 21/09
+    ["kaiserfogo9"]          = true, -- até 21/09
+    ["woonz_x"]              = true, -- até 24/09
+    ["twelo_zinha3"]         = true, -- 26/08 até 26/09
+    ["Gabrielmilitar_6"]     = true, -- 27/08 até 27/12
+    ["Riggs_scott"]          = true, -- 26/08 até 26/09
+    ["01Xxx09"]              = true, -- 29/08 até 12/09
+    ["almahola9"]            = true, -- 29/08 até 21/01/2026
+    ["iwejjbsb1"]            = true, -- 01/09 até 01/01/2026
+    ["WONEVERY1"]            = true, -- 02/09 até 08/09
+    ["mandrake07970"]        = true, -- 03/09 até 23/09
+    ["mini_fofinhodemais"]   = true, -- 05/09 até 08/09
+    ["MANDRAK123459"]        = true, -- 05/09 até 08/09
+    ["biel_mrqs"]            = true, -- 06/09 até 06/01
+    ["BloxLOUX_YT"]          = true, -- 06/09 até 09/09
+    ["CRIA027K"]             = true, -- 07/09 até 19/09
+    ["berluiz2001"]          = true, -- 07/09 até 07/11
+    ["12etdfgj"]             = true, -- 07/09 até 14/09
+    ["ggmax10t"]             = true, -- 07/09 até 07/10
+    ["mellu107"]             = true, -- 07/09 até 10/09
+    ["rylyn15243w0"]         = true, -- 07/09 até 10/09
+    ["MasterReaper111"]      = true, -- 08/09 até 08/01
+    ["LuisCarlos_lcs"]       = true, -- 08/09 até 11/09
+    ["pabinhoplay"]          = true, -- 1 dia
+    ["Theozinhodomorro"]     = true, -- 08/09 até 11/09
+
+    --  Permanentes Uses 🍒
+    ["botinha_W"]            = true,
+    ["21qxkg"]               = true,
+    ["shotxz_cria"]          = true,
+    ["Victhomxd"]            = true,
+    ["aminos_055"]           = true,
+    ["llenado123456"]        = true,
+    ["smulem_01"]            = true,
+    ["Pedrocamandrake391"]   = true,
+    ["KIRA7KILLAS"]          = true,
+    ["daviricardo25022"]     = true,
+    ["Salazar123xxx2"]       = true,
+    ["errorpro1d"]           = true,
+    ["AOEX_FH"]              = true,
+    ["contasegudariadesv"]   = true,
+    ["zoro_fenix42"]         = true,
+    ["pozella999"]           = true,
+    ["nikz68"]               = true,
+    ["kayro20112p"]          = true,
+    ["emanifesto6q"]         = true,
+    ["Lolyta6662"]           = true,
+    ["Migxzz15"]             = true,
+    ["sparta9842"]           = true,
+    ["MENOR_RSA"]            = true,
+    ["dvdfy4wfiyf"]          = true,
 }
 
 return allowedUsers
