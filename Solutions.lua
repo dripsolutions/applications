f814cc260d8086e2ecc00e2e169e3bac4c253bfc
dripsolutions <fcsteikee@gmail.com>
2025-09-08 22:44:37 -0300

    Update Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -50,10 +50,11 @@
     ["MasterReaper111"]      = true, -- 08/09 até 08/01
     ["LuisCarlos_lcs"]       = true, -- 08/09 até 11/09
     ["pabinhoplay"]          = true, -- 1 dia
-    ["ixi362"]               = true, -- 3 dias 08/09 11/09
+    ["ixi362"].              = true, -- 3 dias 08/09 11/09
     ["Theozinhodomorro"]     = true, -- 08/09 até 11/09
     ["ueuruirue"]            = true, -- 08/09 até 11/09
     ["Reibaxaid2"]           = true, -- 08/09 até 11/09
+    ["cantanguandi2"].       =true, -- 08/09 11/09
 
     --  Permanentes Uses 🍒
     ["botinha_W"]            = true,
