2f02657b090ca56587b486c46b91672beed63435
dripsolutions <fcsteikee@gmail.com>
2025-09-13 19:27:47 -0300

    Atualizar o Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -13,6 +13,7 @@
     ["shadow83953"]          = true,
 
     --  Tempos Estimados ⏰
+    ["mosnter_poent"]        = true, -- 13/09 até 03/10
     ["mosnter_poent"]        = true, -- 12/09 até 18/09
     ["luzdespert866"]        = true, -- 11/09 até 26/09
     ["papanoelpro12342"]     = true, -- 04/09 até 20/09
