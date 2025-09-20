ac90da7cd667da8da0efce4ce5b70c3d0489882b
dripsolutions <fcsteikee@gmail.com>
2025-09-20 06:06:45 -0300

    Atualizar o Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -53,7 +53,7 @@
     ["peixebometop"]         = true, -- 19/09 09/10 
     ["brennolindaoel"]       = true, -- 19/09 22/10
     ["chris410"]             = true, -- 19/09 09/10
-    ["hhgfffrgffggf"]        = true, -- 20/09 23/09
+    ["Hhgfffrgffggf"]        = true, -- 20/09 23/09
     --  Permanentes Uses 🍒
     ["botinha_W"]            = true,
     ["21qxkg"]               = true,
