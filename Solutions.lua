f05d8deafebd46e9136cd5583a343c142a023c84
dripsolutions <fcsteikee@gmail.com>
2025-09-21 01:00:51 -0300

    Atualizar o Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -60,6 +60,7 @@
     ["max13434674"]          = true, -- 20/09 23/09
     ["baianim940"]           = true, -- 20/09 23/09
     ["Kenned_Thiago"]        = true, -- 21/09 24/09
+    ["akimanachamana"]       = true, -- 21/09 28/09
     --  Permanentes Uses 🍒
     ["botinha_W"]            = true,
     ["21qxkg"]               = true,
