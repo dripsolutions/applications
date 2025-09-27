cfa078659337058d12196dfc8747e62bbdc24903
dripsolutions <fcsteikee@gmail.com>
2025-09-27 20:47:25 -0300

    Atualizar o Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -79,6 +79,8 @@ allowedUsers = {
     ["PietroPazMaciel"]      = true, -- 27/09 30/09
     ["IVANBR9876"]           = true, -- 27/09 04/10
     ["Thurdanaty"]           = true, -- 27/09 30/09
+    ["luisreal3"]            = true, -- 27/09 04/10
+
 
 
 
