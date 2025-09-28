89eca11040a5f315f333438b5f26fb87908e2964
dripsolutions <fcsteikee@gmail.com>
2025-09-28 11:33:35 -0300

    Atualizar o Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -76,6 +76,10 @@ allowedUsers = {
     ["TBER940"]              = true, -- 28/09 01/10
     ["txmax_5"]              = true, -- 28/09 28/10
     ["Sophia_euoxi"]         = true, -- 28/09 05/10
+    ["TEMPLATE"]             = true, -- 28/09 00/10
+   ["TEMPLATE"]              = true, -- 28/09 00/10
+   ["TEMPLATE"]              = true, -- 28/09 00/10
+
 
     
    ----------///// PERMANENTE ACESS /////------------
