c292efb3eb720150017b4860aa3f359d14db6899
dripsolutions <fcsteikee@gmail.com>
2025-09-28 20:42:07 -0300

    Atualizar o Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -83,7 +83,7 @@ allowedUsers = {
     ["muleke_solitario"]     = true, -- 28/09 30/02
     ["hghbdksmoq45439"]      = true, -- 28/09 28/11
     ["Miguel9827124"]        = true, -- 28/09 2/11
-    ["odiablogames109"]      = true, -- 28/09 30/11
+    ["diablogames109"]      = true, -- 28/09 30/11
     ["Rip_tk1k"]             = true, -- 29/09 4/10
 
 
