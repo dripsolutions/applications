84a267221873a4f4038dfb978ddb6e6696feb452
dripsolutions <fcsteikee@gmail.com>
2025-09-29 01:53:44 -0300

    Atualizar o Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -79,6 +79,7 @@ allowedUsers = {
     ["AntonyRamsdorf"]       = true, -- 28/09 28/10
     ["Lipe1yxz"]             = true, -- 28/09 28/11
     ["EA_BRUH07B"]           = true, -- 28/09 1/10
+    ["silvax0p"]             = true, -- 28/09 1/10
 
 
    ----------///// PERMANENTE ACESS /////------------
