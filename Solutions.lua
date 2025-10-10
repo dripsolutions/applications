1c7ddb834661ad78d9b08a8c0ad9c6fa5277c6a4
dripsolutions <fcsteikee@gmail.com>
2025-10-10 19:45:09 -0300

    Update Solutions.lua

--- Solutions.lua
+++ Solutions.lua
@@ -4,6 +4,7 @@ allowedUsers = {
     ["KcsLaQLbOwB"]          = true,
     ["xsad_9999"]            = true,
     ["cantanguandi2"]        = true,
+    ["matmat2345"]           = true,
     
     
     ----------///// TEMPORARIOS /////-------------
@@ -12,10 +13,9 @@ allowedUsers = {
     ["ERIK90909082"]         = true, -- 02/09 27/12
     ["Gabrielmilitar_6"]     = true, -- 27/08 27/12
     ["Riggs_scott"]          = true, -- 26/08 16/10
-    ["biel_mrqs"]            = true, -- 06/09 6/11
-    ["berluiz2001"]          = true, -- 07/09 7/11
-    ["MasterReaper111"]      = true, -- 08/09 8/11  
-    ["Gustavo194615"]        = true, -- 10/09 10/10
+    ["biel_mrqs"]            = true, -- 06/09 06/11
+    ["berluiz2001"]          = true, -- 07/09 07/11
+    ["MasterReaper111"]      = true, -- 08/09 08/11
     ["darkness_SupremoAWJ"]  = true, -- 10/09 10/11
     ["almahola9"]            = true, -- 30/08 30/12
     ["R190oo"]               = true, -- 15/09 15/11
@@ -42,12 +42,7 @@ allowedUsers = {
     ["rzinxfxp"]             = true, -- 29/09 10/03
     ["1x4_Queijo"]           = true, -- 29/09 29/02
     ["codexxprp1000"]        = true, -- 29/09 29/10
-    ["12muri345"]            = true, -- 01/10 8/10
-    ["ERROR_Death59"]        = true, -- 01/10 8/10
-    ["DouglasWendy21"]       = true, -- 02/10 9/10     
-    ["edu_gachalifeofc"]     = true, -- 03/10 3/12
-    ["davisinhodoroblok"]    = true, -- 04/10 11/10
-    ["tubers_93C00lkid3"]    = true, -- 04/10 10/10
+    ["edu_gachalifeofc"]     = true, -- 03/10 03/12
     ["GuhhsouzzaPP"]         = true, -- 04/10 15/10
 
    ----------///// PERMANENTE ACESS /////------------
